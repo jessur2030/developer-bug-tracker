5365efcd94d75bf56b59859923d22f1a024443a5
jessur2030 <jessur2030@gmail.com>
2022-02-22 23:00:14 -0500

    User register backend functionalities & readme.md

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,11 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
 
-// @desc Register a new user
-// @route /api/users
-// @access Public
+const User = require("../models/userModel");
+
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   //destructured data from the body
   const { name, email, password } = req.body;
@@ -11,13 +14,43 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please include all fields");
   }
+  //Find if user already exits
+  const userExits = await User.findOne({ email });
 
-  res.send("Register user");
+  //if user exits
+  if (userExits) {
+    res.status(400);
+    throw new Error("User already exits");
+  }
+
+  //salt password
+  const salt = await bcrypt.genSalt(10);
+  //hash password
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  //if user is create
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
-// @desc Login a user
-// @route /api/users/login
-// @access Public
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   res.send("Login user");
 });
