69ed596e58c4d3ff8572fe39e22b93e7cf485474
jessur2030 <jessur2030@gmail.com>
2022-02-23 14:43:13 -0500

    implemented user login & jwt functionalities

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/userController");
 
 router.post("/", registerUser);
-router.get("/login", loginUser);
+router.post("/login", loginUser);
 
 module.exports = router;
