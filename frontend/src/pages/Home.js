4617fb6e2678030beb52000f05f2d80d0d821ef8
jessur2030 <jessusdev@gmail.com>
2023-12-06 11:20:23 -0500

    update home page markup

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,14 @@
-import { Link } from "react-router-dom";
-import { FaPlus, FaBug } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { FaPlus, FaBug } from 'react-icons/fa';
 function Home() {
   return (
     <>
       <section className="hero">
-        {" "}
+        {' '}
         <div className="hero-container">
           <div>
-            <h1>Start Tracking your issues</h1>
-            <p>
-              Keep your code clear with logs of work in development, bugs
-              tracked, bug sets, and collaborate with your entire team on code.
-            </p>
+            <h1>Break issues into actionable tasks</h1>
+            <p>Tackle complex issues with task lists and track their status</p>
             <div className="btn-action-container">
               <Link to="/issues" className="btn-action btn-hover btn-action">
                 <FaBug /> View issues
@@ -27,17 +24,6 @@ function Home() {
           <img src="/images/undraw_fixing_bugs.svg" alt="" />
         </div>
       </section>
-      {/* <section className="heading">
-        <h1>Start Tracking your issues</h1>
-        <p>Please choose an option below</p>
-      </section>
-
-      <Link to="/new-issue" className="btn btn-reverse btn-block">
-        <FaPlus /> Create new issue
-      </Link>
-      <Link to="/issues" className="btn btn-block btn-hover">
-        <FaBug /> View all issues
-      </Link> */}
     </>
   );
 }
