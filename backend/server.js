582f12e74a68cab3a4509cce86eb011852bc7a36
jessur2030 <jessusdev@gmail.com>
2024-01-16 01:26:51 -0500

    Refactor server.js file to use single quotes and fix file paths

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
 //calls connectDB: connects to our database
@@ -15,22 +15,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //User Routes
-app.use("/api/users", require("./routes/userRoutes"));
+app.use('/api/users', require('./routes/userRoutes'));
 
 //Issue Routes
-app.use("/api/issues", require("./routes/issueRoutes"));
+app.use('/api/issues', require('./routes/issueRoutes'));
 
 //Serve frontend
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   //set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index/html")
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index/html')
   );
 } else {
-  app.get("/", (req, res) => {
-    res.json({ message: "Welcome to BugTracker API" });
+  app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to BugTracker API' });
   });
 }
 
